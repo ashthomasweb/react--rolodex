d2bf027d4d3e2caed90ec01c6b9e2cb9da0a77ee
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-10-30 16:49:06 -0400

    Adds json fetch
    Fetches json list of users and sets them in state.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
-import React, { Component } from 'react'
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-
   constructor() {
-    super()
+    super();
 
     this.state = {
-      name: "Ashley",
-      string: "Test Me:"
-    }
+      monsters: []
+    };
+  }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((orcs) => this.setState({ monsters: orcs }))
   }
 
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi, {this.state.name},<br />Edit <code>src/App.js</code> and save to reload, please. And thank you!
-        </p>
-        <button onClick={() => this.setState({ name: "Ash Thomas", string: "Success!" })}>{this.state.string}</button>
-        
-      </header>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
       </div>
-
-    )
+    );
   }
-
-
-
-
-
 }
 
 export default App;
