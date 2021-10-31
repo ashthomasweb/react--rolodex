9d4511e79084005260bbf9c54cb988c11682ba0a
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-10-31 19:37:25 -0400

    Adds search input field
    Needs to be made into component and given filter.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: ''
     };
   }
 
@@ -23,6 +24,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input onChange={e => this.setState({ searchField: e.target.value })} type='search' placeholder='search monsters' />
+        <p>{this.state.searchField}</p>
         <CardList monsters={this.state.monsters} />
       </div>
     );
